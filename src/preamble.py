7f0ea1d813b3ff725745d7d1e1c074f62a3222da
jflamant <flamantjf@gmail.com>
2025-03-30 11:11:42 +0200

    fixing if expression in elementary_signals.py + indentation + linting all files

--- src/preamble.py
+++ src/preamble.py
@@ -1,44 +1,27 @@
 # LOAD NECESSARY PYTHON LIBRARIES
 
-import os
 import sys
+
 sys.path.append("../src")
 
-import numpy                         as np
-import matplotlib                    as mpl
-import matplotlib.pyplot             as plt
-import numpy.random                  as npr
-import scipy.special                 as special
-import statsmodels.stats.proportion  as stms
-import pyssht                        as ssht
-import scipy.signal
-import time
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 
 # IMPORT USEFUL FUNCTIONS
 
-from scipy.io                        import wavfile
-
 
 # LOAD THE FUNCTIONS OF THE KRAVCHUK TOOLBOX
-
-from elementary_signals              import the_noisy_chirp, the_noisy_dirac, the_noisy_sine, the_coherent_state, the_noisy_coherent_state
-from elementary_signals              import display_signal
-from pkravchuk_transform             import the_ptransform
-from ssht_transform                  import the_ssht_transform, the_new_transform, the_inverse_transform, rotate_signal, the_spherical_angles
-from kravchuk_display                import planar_display, spherical_display
-from stft_transform                  import the_stft_transform, stft_display
 
 
 # HANDLE APPEARANCE OF THE PLOTS
 
-from mpl_toolkits.axes_grid1         import make_axes_locatable
 
-mpl.rcParams['xtick.labelsize'] = 20;
-mpl.rcParams['ytick.labelsize'] = 20;
-mpl.rcParams['axes.titlesize'] = 20;
-plt.rc('axes', labelsize=22.5);
-plt.rc('legend', fontsize=20);
-plt.rc('text', usetex=True)
-plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
-mpl.rcParams['font.family'] = 'roman'
+mpl.rcParams["xtick.labelsize"] = 20
+mpl.rcParams["ytick.labelsize"] = 20
+mpl.rcParams["axes.titlesize"] = 20
+plt.rc("axes", labelsize=22.5)
+plt.rc("legend", fontsize=20)
+plt.rc("text", usetex=True)
+plt.rc("text.latex", preamble=r"\usepackage{amsmath}")
+mpl.rcParams["font.family"] = "roman"
--- src/stft_transform.py
+++ src/stft_transform.py
@@ -1,76 +1,81 @@
-import numpy             as np
-import scipy.signal      as signal
-import scipy.fft         as fft
+import numpy as np
+import scipy.signal as signal
+import scipy.fft as fft
 import matplotlib.pyplot as plt
 import cmocean
 
-from   matplotlib.pyplot import figure
+from matplotlib.pyplot import figure
+
 
 def the_stft_transform(x, time_t=[]):
-    
     """
     standard Short-Time Fourier Transform with a circular Gaussian window
     """
-    
+
     # x:      complex signal to be analyzed with N+1 samples
     # time_t: time_t: (optional) time range of observation (default: [0,1, ...,N])
-    
+
     if len(time_t) == 0:
-        time_t     = np.arange(len(x)) - int(len(x)//2)
-        
-    dt        = time_t[1]-time_t[0]
+        time_t = np.arange(len(x)) - int(len(x) // 2)
+
+    dt = time_t[1] - time_t[0]
 
     # Gaussian window of unit energy
-    Ng        = len(x)
-    g      = signal.windows.gaussian(Ng, np.sqrt((Ng)/2/np.pi))
-    g      = g/g.sum()
+    Ng = len(x)
+    g = signal.windows.gaussian(Ng, np.sqrt((Ng) / 2 / np.pi))
+    g = g / g.sum()
     # g         = np.pi**(-1/4) * np.exp( - time_t**2 / 2 )
 
     # Compute the Gaussian Short-Time Fourier Transform
-    fx, _, Vx = signal.stft(x, fs = 1/dt, window=g, nperseg=Ng, noverlap=Ng-1, return_onesided=False)
-    
+    fx, _, Vx = signal.stft(
+        x, fs=1 / dt, window=g, nperseg=Ng, noverlap=Ng - 1, return_onesided=False
+    )
+
     # remap the frequencies in a symmetric fashion
     fx = fft.fftshift(fx)
-    Vx = fft.fftshift(Vx,axes = (0,))
-    
+    Vx = fft.fftshift(Vx, axes=(0,))
+
     return Vx, fx
 
 
-def the_stft_zeros(Vx,time_t,fx,feedback = False):
-
+def the_stft_zeros(Vx, time_t, fx, feedback=False):
     # compute the zeros
-    zx, zy    = extr2min(np.abs(Vx))
-    
+    zx, zy = extr2min(np.abs(Vx))
+
     # coordinates of the zeros on the sphre
     zt = time_t[zy]
     zf = fx[zx]
-    
+
     # give some feedback
     if feedback:
-        print('Local minima method has found '+str(zx.shape[0])+' zeros.')
-    
+        print("Local minima method has found " + str(zx.shape[0]) + " zeros.")
+
     return zt, zf
 
 
-def stft_display(Vx,time_t,fx,zt,zf):
-
+def stft_display(Vx, time_t, fx, zt, zf):
     # turn to pulsation for display
-    zw = 2*np.pi*np.array(zf)
+    zw = 2 * np.pi * np.array(zf)
 
-    figure(figsize = (7.5,4));
-    plt.pcolormesh(time_t,2*np.pi*fx,np.log10(np.abs(Vx)), shading='gouraud', cmap = cmocean.cm.deep)
-    plt.scatter(zt,zw,s = 15,color = 'white')
-    plt.xlabel(r'$t$ (s)',fontsize = 30)
-    plt.ylabel(r'$\omega$ (rad.s$^{-1}$)',fontsize = 30)
+    figure(figsize=(7.5, 4))
+    plt.pcolormesh(
+        time_t,
+        2 * np.pi * fx,
+        np.log10(np.abs(Vx)),
+        shading="gouraud",
+        cmap=cmocean.cm.deep,
+    )
+    plt.scatter(zt, zw, s=15, color="white")
+    plt.xlabel(r"$t$ (s)", fontsize=30)
+    plt.ylabel(r"$\omega$ (rad.s$^{-1}$)", fontsize=30)
     plt.tight_layout()
-    
-    
+
+
 def extr2min(M):
-    
     """
     find spectrogram zeros by the Minimal Grid Neighbors method
     """
-    
+
     central = M[1:-1, 1:-1]
     mask = np.full(central.shape, True, dtype=bool)
     sub_indices = (
