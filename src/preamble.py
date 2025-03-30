f80822cc36c1908552d073a1d2f4708757895d02
Barbara Pascal <pascalba92@gmail.com>
2025-03-30 13:01:03 +0200

    without poetry I did not manage to run the notebooks without importing functions

--- src/preamble.py
+++ src/preamble.py
@@ -4,18 +4,29 @@ import sys
 
 sys.path.append("../src")
 
+import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
 # IMPORT USEFUL FUNCTIONS
 
+from scipy.io import wavfile
+
 
 # LOAD THE FUNCTIONS OF THE KRAVCHUK TOOLBOX
+
+from elementary_signals import the_noisy_chirp, the_noisy_dirac, the_noisy_sine, the_coherent_state, the_noisy_coherent_state
+from elementary_signals import display_signal
+from pkravchuk_transform import the_ptransform
+from ssht_transform import the_ssht_transform, the_new_transform, the_inverse_transform, rotate_signal, the_spherical_angles
+from kravchuk_display import planar_display, spherical_display
+from stft_transform import the_stft_transform, stft_display
 
 
 # HANDLE APPEARANCE OF THE PLOTS
 
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 mpl.rcParams["xtick.labelsize"] = 20
 mpl.rcParams["ytick.labelsize"] = 20
